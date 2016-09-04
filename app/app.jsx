52f5427ce658acd611b0c9179a7e14a98d2c10c1
Mireia <mireia@mireiasangalo.com>
2016-09-04 14:06:52 +0100

    Adding foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -2,9 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Announcement = require('Announcement');
 var Main = require('Main');
-
 var TrainSchedule = require('TrainSchedule').configure();
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
 
 ReactDOM.render(
     <div>
